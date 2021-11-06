2019582dcfe157d709db13da4a23825ff5dd78a3
Hodan <you@example.com>
2021-11-06 15:34:24 -0400

    Added first functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named fight
+
+function fight() {
+    window.alert("TThe fight has begun");
+}
+
+var playerName = window.prompt("What is your robot's name?")
+
+//Note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// this will do math and log 20
+console.log(10+10);
+
+//whis is this?
+console.log("Our robot's name is" + playerName);
+
+
+
+// fight();
